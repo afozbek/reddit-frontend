0da03814e16724e7eab527fb1207cebb9355a63e
afozbek <furkanozbek1995@gmail.com>
2020-09-18 17:52:04 +0300

    InputField componenti oluşturuldu

---
+++ src/components/InputField.tsx
@@ -0,0 +1,29 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/core";
+import { useField } from "formik";
+import React from "react";
+
+type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
+      {error && <FormErrorMessage>{error}</FormErrorMessage>}
+    </FormControl>
+  );
+};
