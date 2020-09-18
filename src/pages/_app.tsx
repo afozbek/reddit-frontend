f913dd77a8eca8804f64937d8432cc844787e145
afozbek <furkanozbek1995@gmail.com>
2020-09-18 22:14:44 +0300

    cors için güncellemerl

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,12 @@ import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import theme from "../theme";
 
 import { createClient, Provider } from "urql";
-const client = createClient({ url: "http://localhost:8080/graphql" });
+const client = createClient({
+  url: "http://localhost:8080/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
 
 function MyApp({ Component, pageProps }: any) {
   return (
