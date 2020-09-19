71421af2f15d11c7fd93d059593960f8a6115130
afozbek <furkanozbek1995@gmail.com>
2020-09-19 21:56:26 +0300

    logout mutation generate edildi(type)

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -57,6 +57,7 @@ export type Mutation = {
   deleteUser: Scalars['Boolean'];
   register: UserResponse;
   login?: Maybe<UserResponse>;
+  logout: Scalars['Boolean'];
 };
 
 
@@ -131,6 +132,14 @@ export type LoginMutation = (
   )> }
 );
 
+export type LogoutMutationVariables = Exact<{ [key: string]: never; }>;
+
+
+export type LogoutMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'logout'>
+);
+
 export type RegisterMutationVariables = Exact<{
   username: Scalars['String'];
   password: Scalars['String'];
@@ -184,6 +193,15 @@ export const LoginDocument = gql`
 
 export function useLoginMutation() {
   return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
+};
+export const LogoutDocument = gql`
+    mutation Logout {
+  logout
+}
+    `;
+
+export function useLogoutMutation() {
+  return Urql.useMutation<LogoutMutation, LogoutMutationVariables>(LogoutDocument);
 };
 export const RegisterDocument = gql`
     mutation Register($username: String!, $password: String!) {
