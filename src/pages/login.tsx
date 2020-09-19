6834ca1f9c239fa5007eda0c3d772e69dd3c2f60
afozbek <furkanozbek1995@gmail.com>
2020-09-19 15:36:38 +0300

    login sayfası oluşturuldu

---
+++ src/pages/login.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "./../components/InputField";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+const Login: React.FC<{}> = (props) => {
+  const [, loginMutation] = useLoginMutation();
+  const router = useRouter();
+
+  const handleSubmit = async (values: any, { setErrors }: any) => {
+    const response = await loginMutation({ options: values });
+
+    if (response.data?.login?.errors) {
+      setErrors(toErrorMap(response.data?.login.errors));
+    } else if (response.data?.login?.user) {
+      // worked
+      console.log(response.data?.login.user);
+      router.push("/");
+    }
+  };
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={handleSubmit}
+      >
+        {({ values, handleChange, isSubmitting }) => (
+          <Form>
+            <InputField
+              placeholder="Username"
+              name="username"
+              label="Username"
+            />
+
+            <Box mt="20px">
+              <InputField
+                placeholder="Password"
+                name="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+
+            <Button
+              mt={4}
+              variantColor="teal"
+              // isLoading={isSubmitting}
+              type="submit"
+              cursor="pointer"
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
