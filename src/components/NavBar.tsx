effe1df3c305be1eee9d1176845bbc1db4362db7
afozbek <furkanozbek1995@gmail.com>
2020-09-19 15:36:48 +0300

    navbar oluşturuldu

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Box, Button, Flex, Link } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+  // data is loading
+  if (fetching) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Box>
+        <Flex>
+          <Box mr={2}>Hello {data.me.username}</Box>
+          <Button variant="link" variantColor="white">
+            Log Out
+          </Button>
+        </Flex>
+      </Box>
+    );
+  }
+
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
