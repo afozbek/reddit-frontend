06abdce56200fc2d6edc315b55d4d7b1e6fca102
afozbek <furkanozbek1995@gmail.com>
2020-09-20 13:28:22 +0300

    urqlClient taşındı

---
+++ src/utils/createUrqlClient.ts
@@ -0,0 +1,68 @@
+import { cacheExchange } from "@urql/exchange-graphcache";
+import { dedupExchange, fetchExchange } from "urql";
+import {
+  LoginMutation,
+  MeQuery,
+  MeDocument,
+  RegisterMutation,
+  LogoutMutation,
+} from "../generated/graphql";
+import { betterUpdateQuery } from "./betterUpdateQuery";
+
+export const createUrqlClient = (ssrExchange: any) => ({
+  url: "http://localhost:8080/graphql",
+  fetchOptions: {
+    credentials: "include" as const,
+  },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              result,
+              (res, query) => {
+                if (res.login?.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: res.login?.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              result,
+              (res, query) => {
+                if (res.register.user) {
+                  return query;
+                } else {
+                  return {
+                    me: res.register.user,
+                  };
+                }
+              }
+            );
+          },
+          logout: (result, args, cache, info) => {
+            betterUpdateQuery<LogoutMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              result,
+              () => ({ me: null })
+            );
+          },
+        },
+      },
+    }),
+    ssrExchange,
+    fetchExchange,
+  ],
+});
