5198850d189e8de4424a1e6ccff656580b7c0e13
afozbek <furkanozbek1995@gmail.com>
2020-09-22 22:03:09 +0300

    ana sayfaya create post buton eklendi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
-import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
+import Layout from "./../components/Layout";
+import React from "react";
+import { Box, Button } from "@chakra-ui/core";
+import { useRouter } from "next/router";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
-
+  const router = useRouter();
   return (
-    <>
-      <NavBar />
+    <Layout>
+      <Box margin="auto" width="50%">
+        <Button
+          mt={4}
+          width="100%"
+          variantColor="teal"
+          onClick={() => router.push("/create-post")}
+          cursor="pointer"
+        >
+          Create Post
+        </Button>
+      </Box>
+
+      <br />
       {!data ? (
         <div>Loading...</div>
       ) : (
@@ -18,7 +33,7 @@ const Index = () => {
           ))}
         </ul>
       )}
-    </>
+    </Layout>
   );
 };
 
