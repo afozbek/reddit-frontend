f3f214dc8cb8ae002b1901edf9b9bf94c05a7a57
afozbek <furkanozbek1995@gmail.com>
2020-09-22 22:01:32 +0300

    input field kısmına text area eklendi

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,6 +2,7 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Textarea,
   FormErrorMessage,
 } from "@chakra-ui/core";
 import { useField } from "formik";
@@ -10,19 +11,27 @@ import React from "react";
 type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
+  textArea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
   label,
+  textArea,
   size: _,
   ...props
 }) => {
+  let C = Input;
+
+  if (textArea) {
+    C = Textarea;
+  }
+
   const [field, { error }] = useField(props);
 
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <C {...field} {...props} id={field.name} />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
   );
