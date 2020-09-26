d85e4f407774cf6b669682e2f9f5c39924a9e336
afozbek <furkanozbek1995@gmail.com>
2020-09-26 13:49:10 +0300

    post feature componenti daha düzgün hale getirildi

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,16 +1,45 @@
-import { Box, Heading, Text } from "@chakra-ui/core";
+import { Box, Flex, Heading, Icon, IconButton, Text } from "@chakra-ui/core";
 import React from "react";
+import { Post, User } from "../generated/graphql";
 
 interface FeatureProps {
-  title: string;
-  desc: string;
+  post: Pick<
+    Post,
+    | "id"
+    | "title"
+    | "textSnippet"
+    | "creatorId"
+    | "createdAt"
+    | "updatedAt"
+    | "points"
+  > & {
+    creator: Pick<User, "id" | "username" | "email">;
+  };
 }
 
-export const Feature: React.FC<FeatureProps> = ({ title, desc, ...rest }) => {
+export const Feature: React.FC<FeatureProps> = ({ post, ...rest }) => {
   return (
     <Box p={5} shadow="md" borderWidth="1px" {...rest}>
-      <Heading fontSize="xl">{title}</Heading>
-      <Text mt={4}>{desc}</Text>
+      <Flex>
+        <Box mr={4}>
+          <Flex flexDirection="column" alignItems="center">
+            <IconButton icon="chevron-up" size="lg" aria-label="Vote Up" />
+            <Text padding="5px 0">
+              <strong>{post.points}</strong>
+            </Text>
+            <IconButton icon="chevron-down" size="lg" aria-label="Vote Down" />
+          </Flex>
+        </Box>
+        <Box width="100%">
+          <Flex alignItems="center">
+            <Heading fontSize="xl">{post.title}</Heading>
+            <Box marginLeft="auto">
+              Posted By: <strong>{post.creator.username}</strong>
+            </Box>
+          </Flex>
+          <Text mt={4}>{post.textSnippet}</Text>
+        </Box>
+      </Flex>
     </Box>
   );
 };
