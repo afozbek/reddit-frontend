86f99015b57da13df5e568234b06c980e52da224
afozbek <furkanozbek1995@gmail.com>
2020-09-26 20:33:12 +0300

    voteHandler eklendi

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,22 +1,72 @@
 import { Box, Flex, Heading, Icon, IconButton, Text } from "@chakra-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import { PostSnippetFragment } from "../generated/graphql";
+import { useVoteMutation } from "./../generated/graphql";
 
 interface PostProps {
   post: PostSnippetFragment;
 }
 
 const Post: React.FC<PostProps> = ({ post, ...rest }) => {
+  const [votingLoading, setVotingLoading] = useState<
+    "upvote-loading" | "downvote-loading" | "not-loading"
+  >("not-loading");
+  const [, vote] = useVoteMutation();
+
+  const handleVoting = async (e: any) => {
+    let value = 1;
+    let loadingType: "upvote-loading" | "downvote-loading" | "not-loading" =
+      "not-loading";
+
+    switch (e.target.dataset.voteType) {
+      case "upvote":
+        value = 1;
+        loadingType = "upvote-loading";
+        break;
+      case "downvote":
+        value = -1;
+        loadingType = "downvote-loading";
+        break;
+      default:
+        value = 1;
+        loadingType = "not-loading";
+        break;
+    }
+
+    setVotingLoading(loadingType);
+
+    await vote({
+      postId: post.id,
+      value,
+    });
+
+    setVotingLoading("not-loading");
+  };
+
   return (
     <Box p={5} shadow="md" borderWidth="1px" {...rest}>
       <Flex>
         <Box mr={4}>
           <Flex flexDirection="column" alignItems="center">
-            <IconButton icon="chevron-up" size="lg" aria-label="Vote Up" />
+            <IconButton
+              data-vote-type="upvote"
+              onClick={handleVoting}
+              icon="chevron-up"
+              size="lg"
+              aria-label="Vote Up"
+              isLoading={votingLoading === "upvote-loading"}
+            />
             <Text padding="5px 0">
               <strong>{post.points}</strong>
             </Text>
-            <IconButton icon="chevron-down" size="lg" aria-label="Vote Down" />
+            <IconButton
+              data-vote-type="downvote"
+              onClick={handleVoting}
+              icon="chevron-down"
+              size="lg"
+              aria-label="Vote Down"
+              isLoading={votingLoading === "downvote-loading"}
+            />
           </Flex>
         </Box>
         <Box width="100%">
