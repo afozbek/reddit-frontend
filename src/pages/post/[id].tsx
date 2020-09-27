721ea7ef4c9091f7ca17f71f101c64fde800e001
afozbek <furkanozbek1995@gmail.com>
2020-09-27 14:42:44 +0300

    post sayfasındaki logic düzenlendi

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -4,28 +4,38 @@ import { createUrqlClient } from "../../utils/createUrqlClient";
 import { useRouter } from "next/router";
 import { usePostQuery } from "../../generated/graphql";
 import Layout from "./../../components/Layout";
-import { Spinner } from "@chakra-ui/core";
+import { Box, Heading, Spinner } from "@chakra-ui/core";
 
-interface PostProps {}
-
-export const Post: React.FC<PostProps> = ({}) => {
+const Post: React.FC<{}> = () => {
   const router = useRouter();
 
   const paramPostId =
     typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
 
-  const [{ data, fetching }] = usePostQuery({
+  const [{ data, fetching, error }] = usePostQuery({
     pause: paramPostId === -1,
     variables: {
       postId: paramPostId,
     },
   });
+  let body: any = "";
 
   if (fetching) {
-    return <Spinner />;
+    body = <Spinner />;
+  } else if (!data?.post) {
+    body = <div>Could not find the post</div>;
+  } else if (error) {
+    body = <div>{error.message}</div>;
+  } else {
+    body = (
+      <Box>
+        <Heading mb={4}>{data.post.title}</Heading>
+        <p>{data.post.text}</p>
+      </Box>
+    );
   }
 
-  return <Layout>{data?.post?.text}</Layout>;
+  return <Layout>{body}</Layout>;
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
