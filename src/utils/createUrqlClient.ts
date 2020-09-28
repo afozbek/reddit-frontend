b35203c85e686261f66c78f072ed5b14d4e48a3d
afozbek <furkanozbek1995@gmail.com>
2020-09-28 11:12:39 +0300

    urql cliente condition eklendi

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -77,8 +77,9 @@ export const cursorPagination = (): Resolver => {
 
 export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   let cookie = "";
+
   if (isServer()) {
-    cookie = ctx.req.headers.cookie;
+    cookie = ctx?.req?.headers?.cookie ? ctx?.req?.headers?.cookie : "";
   }
 
   return {
