d5ee47396f32628af71c25964686f97a7ef1a37d
afozbek <furkanozbek1995@gmail.com>
2020-09-30 17:12:36 +0300

    Post içerisinden Voting ayrı componente ayrıldı

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,92 +1,30 @@
 import { Box, Flex, Heading, IconButton, Text, Link } from "@chakra-ui/core";
-import React, { useState } from "react";
+import React from "react";
 import {
   PostSnippetFragment,
   useDeletePostMutation,
   useMeQuery,
 } from "../generated/graphql";
-import { useVoteMutation } from "./../generated/graphql";
 import NextLink from "next/link";
+import { Voting } from "./Voting";
 
 interface PostProps {
   post: PostSnippetFragment;
 }
 
 const Post: React.FC<PostProps> = ({ post, ...rest }) => {
-  const [votingLoading, setVotingLoading] = useState<
-    "upvote-loading" | "downvote-loading" | "not-loading"
-  >("not-loading");
-  const [, vote] = useVoteMutation();
   const [{ data }] = useMeQuery();
   const [, deletePost] = useDeletePostMutation();
-
-  const handleVoting = async (voteType: "upvote" | "downvote") => {
-    let value = 1;
-    let loadingType: "upvote-loading" | "downvote-loading" | "not-loading" =
-      "not-loading";
-    switch (voteType) {
-      case "upvote":
-        if (post.voteStatus === 1) {
-          return;
-        }
-
-        value = 1;
-        loadingType = "upvote-loading";
-
-        break;
-      case "downvote":
-        if (post.voteStatus === -1) {
-          return;
-        }
-
-        value = -1;
-        loadingType = "downvote-loading";
-        break;
-      default:
-        value = 1;
-        loadingType = "not-loading";
-        break;
-    }
-
-    setVotingLoading(loadingType);
-
-    await vote({
-      postId: post.id,
-      value,
-    });
-
-    setVotingLoading("not-loading");
-  };
 
   return (
     <Box p={5} shadow="md" borderWidth="1px" {...rest}>
       <Flex>
-        <Box mr={4}>
-          <Flex flexDirection="column" alignItems="center">
-            <IconButton
-              data-vote-type="upvote"
-              onClick={() => handleVoting("upvote")}
-              icon="chevron-up"
-              size="lg"
-              variantColor={post.voteStatus === 1 ? "teal" : undefined}
-              aria-label="Vote Up"
-              isLoading={votingLoading === "upvote-loading"}
-            />
-            <Text padding="5px 0">
-              <strong>{post.points}</strong>
-            </Text>
+        <Voting
+          postId={post.id}
+          voteStatus={post.voteStatus}
+          points={post.points}
+        />
 
-            <IconButton
-              data-vote-type="downvote"
-              onClick={() => handleVoting("downvote")}
-              icon="chevron-down"
-              size="lg"
-              variantColor={post.voteStatus === -1 ? "red" : undefined}
-              aria-label="Vote Down"
-              isLoading={votingLoading === "downvote-loading"}
-            />
-          </Flex>
-        </Box>
         <Box width="100%">
           <Flex alignItems="center" mb={2}>
             <NextLink href="/post/[id]" as={`/post/${post.id}`}>
