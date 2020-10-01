b33a542a5baf063bad2e85e8ea26092004c38c90
afozbek <furkanozbek1995@gmail.com>
2020-10-01 23:44:34 +0300

    port 4000 den dinlenmeye başlandı

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -93,7 +93,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   }
 
   return {
-    url: "http://localhost:8080/graphql",
+    url: "http://localhost:4000/graphql",
     fetchOptions: {
       credentials: "include" as const,
       headers: cookie
