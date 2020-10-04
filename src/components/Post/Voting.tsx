79eefeb4ea8b5105de01fbf8467ea6e7bfbede4f
afozbek <furkanozbek1995@gmail.com>
2020-10-04 14:40:16 +0300

    voting cache işlemleri yapıldı

--- src/components/Post/Voting.tsx
+++ src/components/Post/Voting.tsx
@@ -1,6 +1,7 @@
+import { ApolloCache, gql } from '@apollo/client';
 import { Box, Flex, IconButton, Text } from '@chakra-ui/core';
 import React, { useState } from 'react';
-import { useVoteMutation } from '../../generated/graphql';
+import { Post, useVoteMutation, VoteMutation } from '../../generated/graphql';
 
 interface VotingProps {
   voteStatus: number | null | undefined;
@@ -18,6 +19,42 @@ export const Voting: React.FC<VotingProps> = ({
   >('not-loading');
 
   const [vote] = useVoteMutation();
+
+  const updateAfterVote = (value: number, cache: ApolloCache<VoteMutation>) => {
+    // todo
+    const data = cache.readFragment<{
+      id: number;
+      points: number;
+      voteStatus: number | null;
+    }>({
+      id: 'Post:' + postId,
+      fragment: gql`
+        fragment _ on Post {
+          id
+          points
+          voteStatus
+        }
+      `,
+    });
+
+    if (data) {
+      if (data.voteStatus === value) {
+        return;
+      }
+
+      const newPoints = data.points + (!data.voteStatus ? 1 : 2) * value;
+      cache.writeFragment({
+        id: 'Post:' + postId,
+        fragment: gql`
+          fragment __ on Post {
+            points
+            voteStatus
+          }
+        `,
+        data: { points: newPoints, voteStatus: value },
+      });
+    }
+  };
 
   const handleVoting = async (voteType: 'upvote' | 'downvote') => {
     let value = 1;
@@ -51,6 +88,7 @@ export const Voting: React.FC<VotingProps> = ({
 
     await vote({
       variables: { postId, value },
+      update: (cache) => updateAfterVote(value, cache),
     });
 
     setVotingLoading('not-loading');
