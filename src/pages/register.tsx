23d3f398543ac11a4e13cd5b977a2d200a2cba01
afozbek <furkanozbek1995@gmail.com>
2020-10-04 14:38:32 +0300

    register import güncellendi

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,13 +4,10 @@ import { Form, Formik } from 'formik';
 import { Box, Button } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from './../components/InputField';
-import { useRegisterMutation } from '../generated/graphql';
+import { MeDocument, MeQuery, useRegisterMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from 'next/router';
-import { withUrqlClient } from 'next-urql';
-import { createUrqlClient } from '../utils/createUrqlClient';
 import Layout from '../components/Layout';
-import { UserInput } from './../../../reddit-server/src/types/UserInput';
 import { withApollo } from '../utils/withApollo';
 
 interface Props {}
